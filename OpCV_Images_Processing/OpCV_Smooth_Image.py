6e45ee7c785acb60b1fb40544434d73d072647bb
Vincent <20222005026@m.scnu.edu.cn>
2024-03-05 15:19:22 +0800

    Smooth_Image add two_dimensional_convolution function

--- OpCV_Images_Processing/OpCV_Smooth_Image.py
+++ OpCV_Images_Processing/OpCV_Smooth_Image.py
@@ -2,6 +2,19 @@ import cv2 as cv
 import numpy as np
 from matplotlib import pyplot as plt
 
-def blur_image():
-    print('b')
+def two_dimensional_convolution():
+    img = cv.cvtColor(cv.imread('..\\src\\Rika_cry.png'), cv.COLOR_BGR2RGB)
+    kernel = np.ones((5,5),np.float32)/25
+    dst = cv.filter2D(img, -1, kernel=kernel)
+    plt.subplot(121), plt.imshow(img), plt.title('Original')
+    plt.xticks([]), plt.yticks([])
+    plt.subplot(122), plt.imshow(dst), plt.title('2D dimensional')
+    plt.xticks([]), plt.yticks([])
+    plt.show()
+
+
+
+
+if __name__ == '__main__' :
+    #two_dimensional_convolution()
     pass
